1aafa24db568d14e8b8df7abd3ad9a432ced541a
ManuelLazaro <gorselazarorosas@gmail.com>
2025-01-15 17:11:10 -0500

    IA

---
+++ src/main/java/OpenIA/IaTeamApplication.java
@@ -0,0 +1,13 @@
+package OpenIA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IaTeamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IaTeamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/OpenIA/IaTeamApplicationTests.java
@@ -0,0 +1,13 @@
+package OpenIA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IaTeamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
